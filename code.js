a26585bd755b2829683cfa57204bc1a1e074a82c
Swati <swatic019@gmail.com>
2023-01-12 08:24:35 +0530

    opearated every key of calculator

--- code.js
+++ code.js
@@ -1,37 +1,28 @@
-let display = document.getElementById("numfield");
-let buttons = Array.from(document.getElementsByClassName("number"));
-let numClick = document.querySelectorAll(".number");
-let equal = document.getElementById("equal");
-
-let prevValue = 0;
-let nextValue = "";
-let operatorGiven = false;
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    display.value += btn.innerText;
-    if (display.value == "") {
-      display.value = 0;
-     
-    } 
-   
-   
-   if (btn.innerText == "AC") {
-      display.value = 0;
-      prevValue = "";
-      operatorGiven = false;
-      nextValue = "";
-      return;
+let keys = document.querySelector(".calculator_keys");
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    let key = e.target;
+    let action = key.dataset.acion;
+    
+    if (!action) {
+      console.log("number key");
     }
-  });
-});
-
-equal.addEventListener('click', ()=>{
-    try {
-        let ans = eval(display.value);
-        display.value = ans;
-        
-    } catch (error) {
-           
+    
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key");
     }
+
+    if(action === 'clear'){
+      console.log('clear key');
+    }
+
+    if(action === 'calculate'){
+      console.log('equal key');
+    }
+  }
 });
